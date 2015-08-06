adc41ae4196a7a0cf16a6fbf2bf7a0e5a3412148
Scott Richards <scott.srichards@gmail.com>
2015-08-06 11:08:18 -0700

    Initial Commit

---
+++ AcronymLookup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AcronymLookup
+//
+//  Created by Scott Richards on 8/6/15.
+//  Copyright (c) 2015 Scott Richards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AcronymLookup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcronymLookup
+//
+//  Created by Scott Richards on 8/6/15.
+//  Copyright (c) 2015 Scott Richards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
